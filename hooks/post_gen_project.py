1eb67593691ee6b0bb655d432a5bbea5368f792f
Ben Shippey <shippeyben@gmail.com>
2024-04-02 09:16:54 -0400

    Commit

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,3 +1,4 @@
 import os
 
 os.system('code "../{{cookiecutter.project_name}} ({{cookiecutter.requester}}) ({{cookiecutter.date}})/Quick Notes.code-workspace"')
+print("\n\n********\nProject Files generated\n********")
