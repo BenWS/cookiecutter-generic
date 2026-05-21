8fa418ad3ca4c6f27b03d3c1ab183232b9c8b804
BWS1796_thdgit <benjamin_w_shippey@homedepot.com>
2026-05-21 16:58:25 -0400

    Removing post-hook

--- hooks/post_gen_project.py
+++
@@ -1,4 +0,0 @@
-import os
-
-os.system('code "../{{cookiecutter.project_name}} ({{cookiecutter.requester}}) ({{cookiecutter.date}})/Quick Notes.code-workspace"')
-print("\n\n********\nProject Files generated\n********")
